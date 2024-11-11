646838105291d2295b4f82b5be301e2d56abdce3
Gleb Yasynskyi <gleb.yasinsky@gmail.com>
2024-11-11 22:03:52 +0200

    hw7 - swagger

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 // import { logger } from './middlewares/logger.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -19,6 +20,7 @@ export const startServer = () => {
   app.use(express.json());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.get('/', (req, res) => {
     res.json({
